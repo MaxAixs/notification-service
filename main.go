a181f4b3cf3c3df45931d2319fcd04c4f1c101a0
Aix <Aix60689100@gmail.com>
2025-02-24 19:24:36 +0200

    add gRPC client and analytics worker to notification service

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"notification-service/cmd/server"
+	"notification-service/internal/config"
 	"notification-service/notification/handler"
 	"notification-service/notification/repository"
 	"notification-service/notification/service"
+	"notification-service/pkg/analyticService/client"
+	"notification-service/pkg/analyticService/worker"
 	"notification-service/pkg/database"
 	"notification-service/pkg/mailgun"
 	"os"
@@ -20,7 +23,7 @@ import (
 func main() {
 	logrus.SetFormatter(&logrus.JSONFormatter{})
 
-	if err := initConfig(); err != nil {
+	if err := config.InitCfg(); err != nil {
 		logrus.Fatalf("init config failed: %v", err)
 	}
 
@@ -57,11 +60,13 @@ func main() {
 
 	srv := &server.Server{}
 	srvCfg := server.ConfigServer{
-		Port:              port,
+		Port:              config.GetPort(),
 		ReadHeaderTimeout: viper.GetDuration("server.read_header_timeout"),
 		WriteTimeout:      viper.GetDuration("server.write_timeout"),
 		IdleTimeout:       viper.GetDuration("server.idle_timeout"),
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
 
 	go func() {
 		if err := srv.RunServer(srvCfg, handlers.MapRoutes()); err != nil {
@@ -71,24 +76,32 @@ func main() {
 
 	logrus.Println("notification server started")
 
+	gRPCClient, err := client.NewAnalyticsClient(viper.GetString("analytics.grpc_port"))
+	if err != nil {
+		logrus.Fatalf("failed to create analytics client: %v", err)
+	}
+	defer gRPCClient.CloseConnection()
+
+	analyticWorker := worker.NewAnalyticWorker(services, gRPCClient)
+
+	go func() {
+		if err := analyticWorker.Start(ctx); err != nil {
+			logrus.Fatalf("failed to start analytics worker: %v", err)
+		}
+	}()
+
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	cancel()
 
-	if err := srv.ShutDown(ctx); err != nil {
+	shutDownCtx, shutDownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutDownCancel()
+
+	if err := srv.ShutDown(shutDownCtx); err != nil {
 		logrus.Fatalf("failed to shutdown notification server: %v", err)
 	}
 
 	logrus.Println("notification server shutdown")
-}
-
-func initConfig() error {
-	viper.AddConfigPath("internal/config")
-	viper.SetConfigName("config")
-	viper.SetConfigType("yml")
-
-	return viper.ReadInConfig()
 }
